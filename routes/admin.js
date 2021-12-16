1e8693a90d8b72b859a4dd29b394588eb0ed8824
hung dao <hungpepi2001@gmail.com>
2021-12-16 10:43:51 +0700

    add model

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,15 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    path: "/admin/add-product",
-    pageTitle: "Add Product",
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
