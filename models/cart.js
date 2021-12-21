9a9d7a4d0943bc69de9c08acf93e052a86ceac04
hung dao <hungpepi2001@gmail.com>
2021-12-21 15:13:21 +0700

    add to cart

--- models/cart.js
+++ models/cart.js
@@ -31,7 +31,7 @@ module.exports = class Cart {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      cart.totalPrice = cart.totalPrice + +productPrice;
+      cart.totalPrice += +productPrice;
       fs.writeFile(p, JSON.stringify(cart), (err) => {
         console.log(err);
       });
--- models/product.js
+++ models/product.js
@@ -18,20 +18,32 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
+    this.id = id;
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -41,6 +53,18 @@ module.exports = class Product {
   static findById(id, cb) {
     getProductsFromFile((products) => {
       const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
+
+  static deleteById(id, cb) {
+    getProductsFromFile((products) => {
+      const updatedProduct = products.filter((p) => p.id !== id);
+      fs.writeFile(id, JSON.stringify(updatedProduct), (err) => {
+        if (!err) {
+          
+        }
+      });
       cb(product);
     });
   }
